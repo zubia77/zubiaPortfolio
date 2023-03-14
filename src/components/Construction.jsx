7a6dfbd11966c6d9d007af17e2d30cbae39d2c43
Zubia Rashid <rashzubia@gmail.com>
2023-03-14 20:16:15 +0100

    settings for responsive

--- src/components/Construction.jsx
+++ src/components/Construction.jsx
@@ -10,7 +10,7 @@ const Construction = () => {
         {/* Bubbles */}
        <Bubbles/>
         {/* Meta Content */}
-        <div className="mainContent bg-color-20/90 m-8 z-10 col-start-1 col-end-11 row-start-2 row-end-4 rounded-xl">
+        <div className="mainContent bg-color-20/90 m-8 z-10 col-start-1 col-end-11 row-start-2 row-end-5 rounded-xl">
           <div className="h-full flex flex-col items-center">
             <img src="../../public/underConstruction.png" alt="" className="lg:w-[800px]" />
             <div className="text-color-10 text-5xl font-10 text-center">Coming Soon</div>
