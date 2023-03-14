8d1eccfb572b14b14a47218d886ee2be770eef7b
Zubia Rashid <rashzubia@gmail.com>
2023-03-14 21:10:59 +0100

    responsive settings

--- src/components/Construction.jsx
+++ src/components/Construction.jsx
@@ -5,13 +5,13 @@ import Bubbles from "../components/Bubbles";
 
 const Construction = () => {
   return (
-    <div className="landingpage  bg-color-10">
+    <div className="bg-color-10">
       <div className="h-screen grid grid-cols-9 grid-rows-5">
         {/* Bubbles */}
        {/* <Bubbles/> */}
         {/* Meta Content */}
-        <div className="mainContent bg-color-20/90 m-8 z-10 col-start-1 col-end-11 row-start-2 row-end-5 rounded-xl">
-          <div className="h-full flex flex-col items-center">
+        <div className="mainContent bg-color-20/90 m-8 z-10 col-start-1 col-end-11 row-start-1 row-end-6 rounded-xl">
+          <div className="h-full flex flex-col items-center justify-center">
             <img src="../../public/underConstruction.png" alt="" className="lg:w-[800px]" />
             <div className="text-color-10 text-5xl font-10 text-center">Coming Soon</div>
           </div>
